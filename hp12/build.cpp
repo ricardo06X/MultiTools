419a1b1f34f1e752b88da3ad32f6f36ef939cc39
ricardo06X <ricardo06@gmail.com>
2026-03-12 09:15:37 -0300

    Delete hp12 directory

--- hp12/build.cpp
+++
@@ -1,7 +0,0 @@
-#include "hp12c.h"
-unsigned long long const BuildDate
-#ifdef _ARM_
-#pragma section = "version"
-@ "version"
-#endif
-= 0x7108202000000000;
--- hp12/prog.tcl
+++
@@ -1,3 +0,0 @@
-send_file {Flash} "HP12C+.bin" 0x100000 0
-compare_file  {Flash} "HP12C+.bin" 0x100000 0
-FLASH::ScriptGPNMV 1
